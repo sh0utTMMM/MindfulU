59174cf78d63cbe37af64cab1bb8d16b298eb50d
Ivan <ivan.georgiev2002k@gmail.com>
2023-05-19 19:36:29 +0200

    Finalizing Character Page

---
+++ app/src/main/java/com/example/mooddetection/TipsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mooddetection
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TipsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tips_page)
+    }
+}
